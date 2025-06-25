db76434588af97acde9e597448aeb8533a0fed8f
Максим Минин <maxonbbbrt@yandex.ru>
2025-06-25 13:49:01 +0300

    Correct

--- shop_project/store/models.py
+++ shop_project/store/models.py
@@ -65,7 +65,7 @@ class Order(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='processing')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PROCESSING')
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
